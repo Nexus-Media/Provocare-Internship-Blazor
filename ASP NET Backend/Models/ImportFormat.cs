bf77658f9c190b4d65abd75d87df28343d007d0a
Camelia1234567890 <cazacucamelia115@gmail.com>
2024-03-14 13:49:39 +0200

    Added backend project

---
+++ ASP NET Backend/Models/ImportFormat.cs
@@ -0,0 +1,10 @@
+public class ImportFormat
+{
+    public int Nr_Document {get;set;}
+    public DateTime Data_Document { get; set; }
+    public string Tip_Plata { get; set; }
+    public string Moneda { get; set; }
+    public decimal Suma { get; set; }
+    public string Iban { get; set; }
+    public string Explicatii { get; set; }
+}
